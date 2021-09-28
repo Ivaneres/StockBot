510fe92fc0238fca982777d48457010aaf48dde9
Ivan Ereshchenko <ereshchenkoi@gmail.com>
2021-09-28 13:12:36 +0100

    Integrated webmonitors with discord bot

--- discord/bot.py
+++ discord/bot.py
@@ -1,21 +1,21 @@
 import logging
-from os import listdir
-from typing import List
+import os
 
 import discord as discord
 import yaml
 from discord.ext import commands, tasks
 
-from data.monitor import StockMonitor
+from data.webmonitor import WebMonitor
 
 
 class StockBot(commands.Bot):
 
-    def __init__(self, config_path: str, monitors: List[StockMonitor], **kwargs):
+    def __init__(self, config_path: str, **kwargs):
         self.config = self.load_config(config_path)
-        self.monitors = monitors
+        sources_path = os.path.normpath(self.config["sources_path"])
+        self.monitors = [WebMonitor.from_yaml(sources_path + "/" + file) for file in os.listdir(sources_path)]
         super().__init__(command_prefix=self.config["command_prefix"], **kwargs)
-        self.load_cogs("./cogs")
+        self.load_cogs("./discord/cogs")
         logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(message)s')
         logging.info("Started bot")
         self.check_stock.start()
@@ -26,12 +26,12 @@ class StockBot(commands.Bot):
             return yaml.safe_load(fp)
 
     def load_cogs(self, path):
-        for file in listdir(path):
+        for file in os.listdir(path):
             if not file.endswith(".py") or file.startswith("__init__"):
                 continue
             self.load_extension(f"{path.replace('./', '')}.{file.replace('.py', '')}")
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=30)
     async def check_stock(self):
         notifs_channel = await self.fetch_channel(self.config["notifs_channel"])
         for monitor in self.monitors:
