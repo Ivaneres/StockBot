9b0713270d960fd550bec1a2e022660ad834eda1
Can Narin <cannarin025@gmail.com>
2021-09-29 16:35:29 +0100

    updated stock cog to use threads and link

--- discord_bot/cogs/stock.py
+++ discord_bot/cogs/stock.py
@@ -6,22 +6,9 @@ class Stock(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def check_stock(self):
-        stock = []
-        for monitor in self.bot.monitors:
-            stock.append(monitor.run())
-        return [x for listing in stock for x in listing]
-
     @commands.command(name="list", help = "displays GPU stock upon request")
     async def list_stock(self, ctx):
-        notifs_channel = await self.bot.fetch_channel(self.bot.config["notifs_channel"])
-        await notifs_channel.send("listing stock!")
-        stock = await self.check_stock()
-        for item in stock:
-            if not item.in_stock:
-                continue
-            await notifs_channel.send(f"Item {item.name} is currently in stock.")
-
+        await self.bot.display_stock()
 
 def setup(bot):
     bot.add_cog(Stock(bot))
